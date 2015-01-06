8c064166ad7c244c85c1f4aa4a671f324c9d1b5a
Noëmie <Nomie@172.16.125.104>
2015-01-06 15:52:43 +0100

    Création de la classe characters

---
+++ src/mr_kitten/Characters.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mr_kitten;
+
+/**
+ *
+ * @author Noëmie
+ */
+public class Characters {
+    
+    private String name;
+    private int ennemiHP;
+    private int ennemiAD;
+    private String talk;
+    
+    public Characters(String n, int hp, int ad, String t){
+        name=n;
+        ennemiHP=hp;
+        ennemiAD=ad;
+        talk=t;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getEnnemiHP(){
+        return ennemiHP;
+    }
+    
+    public int getEnnemiAD() {
+        return ennemiAD;
+    }
+    public String getTalk() {
+        return talk;
+    }
+    
+}
